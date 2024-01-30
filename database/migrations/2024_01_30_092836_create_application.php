963543698c7771b5d481f980352269a2f76897aa
XopK <dmahmutov12@gmail.com>
2024-01-30 14:46:39 +0500

    Update 2024_01_30_092836_create_application.php

--- database/migrations/2024_01_30_092836_create_application.php
+++ database/migrations/2024_01_30_092836_create_application.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('application', function (Blueprint $table) {
+        Schema::create('applications', function (Blueprint $table) {
             $table->id();
             $table->string('comment');
             $table->date('start_date');
@@ -27,6 +27,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('application');
+        Schema::dropIfExists('applications');
     }
 };
