1b3d1e77856d6a9e444a665e61839d9ca3887b3a
Santality <saintly231@gmail.com>
2024-02-09 12:24:58 +0500

    sex3

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -286,9 +286,7 @@
                     </table>
                 </div>
                 <h1 class="h2">Последние добавленые процедуры</h1>
-                <div class="procedures">
-                    <div class="container" style="margin-bottom: 50px">
-                        <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 g-3 mt-3">
+                <div class="procedures row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 g-3 mt-3">
                             <div class="col d-flex justify-content-center">
                                 <a href="#" style="text-decoration: none">
                                     <div class="card shadow border-0" style="width: 18rem;">
@@ -391,8 +389,6 @@
                                     </div>
                                 </a>
                             </div>
-                        </div>
-                    </div>
                 </div>
             </main>
         </div>
